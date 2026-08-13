3ef9401c3bc27fadc630c7a4fb66d36c79bd476c
Joshua Crane <Joshua.crane.me@gmail.com>
2026-08-13 16:52:39 -0400

    contact mobile support and icon change

--- src/components/pages/Contact.js
+++ src/components/pages/Contact.js
@@ -1,24 +1,28 @@
 import React, { useEffect } from 'react';
 
 export default function Contact() {
-  useEffect(() => {
-    const init = () => {
-      if (window.Calendly) {
-        window.Calendly.initInlineWidget({
-          url: 'https://calendly.com/cranehardwoodflooring/estimate?hide_event_type_details=1&hide_gdpr_banner=1',
-          parentElement: document.getElementById('calendly-inline'),
-        });
-      } else {
-        setTimeout(init, 300);
-      }
-    };
-    init();
-  }, []);
+    useEffect(() => {
+        const init = () => {
+            const container = document.getElementById('calendly-inline');
 
-  return (
-    <div style={{ paddingTop: '80px', paddingBottom: '5rem', display: 'flex', flexDirection: 'column', alignItems: 'center', paddingLeft: '0rem', paddingRight: '1rem', boxSizing: 'border-box', width: '100%' }}>
-      <p className="title spacing2">Set Appointment</p>
-      <div id="calendly-inline" style={{ width: '100%', maxWidth: '700px', minHeight: '1050px' }}></div>
-    </div>
-  );
+            if (window.Calendly && container) {
+                window.Calendly.initInlineWidget({
+                    url: 'https://calendly.com/cranehardwoodflooring/estimate?hide_event_type_details=1&hide_gdpr_banner=1',
+                    parentElement: container,
+                });
+            } else {
+                setTimeout(init, 300);
+            }
+        };
+
+        init();
+    }, []);
+
+    return (
+        <div className="contact-page">
+            <p className="title spacing2">Set Appointment</p>
+
+            <div id="calendly-inline" />
+        </div>
+    );
 }
